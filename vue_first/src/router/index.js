6260c5325c00fb7b6e77ddc97f50912355adef9c
HildaWoff <2134801282@qq.com>
2026-02-19 22:40:39 +0800

    第二阶段

--- vue_first/src/router/index.js
+++ vue_first/src/router/index.js
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import Recognize from '../views/Recognize.vue'
-import Camera from "@/views/Camera.vue";
+import Camera from "../views/Camera.vue"
+import Logs from "../views/Logs.vue"
 
 const routes = [
   { path: '/', component: Login },
   { path: '/register', component: Register },
   { path: '/recognize', component: Recognize },
-  { path: '/camera', component: Camera}
+  { path: '/camera', component: Camera},
+  { path: '/logs', component: Logs}
 ]
 
 const router = createRouter({
